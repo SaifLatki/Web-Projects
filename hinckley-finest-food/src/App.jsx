629f69d7d36d60338cc86a7101418add144628d5
Saif <saifullahlatki338@gmail.com>
2025-04-04 19:49:43 +0500

    issue resolve

--- hinckley-finest-food/src/App.jsx
+++ hinckley-finest-food/src/App.jsx
@@ -1,6 +1,6 @@
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Hours from './pages/Hours';
